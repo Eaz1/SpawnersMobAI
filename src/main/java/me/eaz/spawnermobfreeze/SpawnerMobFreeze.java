f764c25b31cc20a60f9921da260bb653146e2b85
Eaz1 <yeabruh652@gmail.com>
2026-07-10 10:52:52 -0700

    Freeze spiders and cave spiders in mid-air

--- src/main/java/me/eaz/spawnermobfreeze/SpawnerMobFreeze.java
+++ src/main/java/me/eaz/spawnermobfreeze/SpawnerMobFreeze.java
@@ -1,26 +1,47 @@
 package me.eaz.spawnermobfreeze;
 
+import org.bukkit.World;
+import org.bukkit.entity.CaveSpider;
 import org.bukkit.entity.Creeper;
-import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Spider;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
-import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class SpawnerMobFreeze extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("SpawnerMobFreeze enabled!");
-    }
 
-    @EventHandler
-    public void onMobSpawn(CreatureSpawnEvent event) {
-        // Do nothing - all mobs keep their normal AI.
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (World world : getServer().getWorlds()) {
+                    for (Entity entity : world.getEntities()) {
+
+                        if (!(entity instanceof Spider) && !(entity instanceof CaveSpider)) {
+                            continue;
+                        }
+
+                        if (entity.isOnGround()) {
+                            continue;
+                        }
+
+                        Vector velocity = entity.getVelocity();
+                        velocity.setY(-0.4);
+                        entity.setVelocity(velocity);
+                    }
+                }
+            }
+        }.runTaskTimer(this, 1L, 1L);
+
+        getLogger().info("SpawnerMobFreeze enabled!");
     }
 
     @EventHandler
@@ -32,7 +53,7 @@ public class SpawnerMobFreeze extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onSpiderTarget(EntityTargetEvent event) {
-        if (event.getEntity() instanceof Spider) {
+        if (event.getEntity() instanceof Spider || event.getEntity() instanceof CaveSpider) {
             event.setCancelled(true);
         }
     }
